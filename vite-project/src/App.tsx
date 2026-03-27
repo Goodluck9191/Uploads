39d4fcac8c55e79a13cd4af097d122c67628e662
Goodluck <goodluckprosper76@gmail.com>
2026-03-28 00:10:55 +0300

    Add the authentication page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,17 @@
 
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 
 
 
 export default function App() {
 	return (
 		<div className="hero">
-
-      <HomePage />
-
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/auth" element={<AuthPage />} />
+			</Routes>
 		</div>
 	);
 }
