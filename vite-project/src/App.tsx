520a75259409f8a50747394d0933bc6e25085f04
Goodluck <goodluckprosper76@gmail.com>
2026-03-28 02:41:42 +0300

    Add the protected routes

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,15 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import { AuthProvider } from "./context/AuthProvider";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 export default function App() {
 	return (
 		<div className="hero">
 			<AuthProvider>
 				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/auth" element={<AuthPage />} />
+					<Route
+						path="/"
+						element={
+							<ProtectedRoute>
+								<HomePage />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						path="/auth"
+						element={
+							<PublicRoute>
+								<AuthPage />
+							</PublicRoute>
+						}
+					/>
 				</Routes>
 			</AuthProvider>
 		</div>
---
+++ vite-project/src/components/PublicRoute.tsx
@@ -0,0 +1,14 @@
+import  { type JSX } from 'react'
+import { useAuth } from '../context/AuthContext'
+import { Navigate } from 'react-router-dom'
+
+const PublicRoute = ({children}:{children: JSX.Element}) => {
+    const { user } = useAuth()
+
+    if(user) {
+        return <Navigate to={'/'} replace/>
+    }
+  return children
+}
+
+export default PublicRoute
